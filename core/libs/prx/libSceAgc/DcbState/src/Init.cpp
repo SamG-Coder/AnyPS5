9286953949e4d4fe14da0a033cc90dee259ab6c9
SamG-Coder <13449963+SamG-Coder@users.noreply.github.com>
2026-09-26 14:54:59 +1000

    fix(agc): separate named and state initialization exports

--- core/libs/prx/libSceAgc/DcbState/src/Init.cpp
+++ core/libs/prx/libSceAgc/DcbState/src/Init.cpp
@@ -8,7 +8,13 @@
 
 extern "C" {
 
-APS5_EXPORT("23LRUSvYu1M", sceAgcInit);
+APS5_EXPORT("23LRUSvYu1M", sceAgc_23LRUSvYu1M);
+int APS5_VABI sceAgc_23LRUSvYu1M(std::uint32_t* state, std::uint32_t version) {
+    Agc::Command::CheckAddress(reinterpret_cast<std::uintptr_t>(state), alignof(std::uint32_t), __func__);
+    Agc::Command::Require(version < 14, __func__, "unsupported AGC initialization version");
+    return 0;
+}
+
 int APS5_VABI sceAgcInit(std::uint32_t version) {
     Agc::Command::Require(version < 14, __func__, "unsupported AGC initialization version");
     return 0;
