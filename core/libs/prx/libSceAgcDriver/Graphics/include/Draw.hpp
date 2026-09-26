341f7875cf28057fe5d2185bb8431035e36b7a8c
Sam <13449963+SamG-Coder@users.noreply.github.com>
2026-09-26 19:04:08 +1000

    refactor(agc): remove PM4 dependency from graphics draw backend

--- core/libs/prx/libSceAgcDriver/Graphics/include/Draw.hpp
+++ core/libs/prx/libSceAgcDriver/Graphics/include/Draw.hpp
@@ -2,11 +2,11 @@
 #define CORE_LIBS_PRX_LIBSCEAGCDRIVER_GRAPHICS_INCLUDE_DRAW_HPP
 
 #include "prx/libSceAgcDriver/Graphics/include/Pipeline.hpp"
-#include "prx/libSceAgcDriver/Execution/include/Pm4.hpp"
+#include "prx/libSceAgcDriver/Graphics/include/DrawParameters.hpp"
 
 namespace AgcDriver::Graphics {
 
-void Draw(const Context& context, const State& state, const Pm4::DrawParameters& draw, std::span<const CompiledShader> shaders, std::span<const GuestMemorySnapshot> snapshots = {});
+void Draw(const Context& context, const State& state, const DrawParameters& draw, std::span<const CompiledShader> shaders, std::span<const GuestMemorySnapshot> snapshots = {});
 
 }
 
